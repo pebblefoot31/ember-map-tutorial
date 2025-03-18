ddbcc04b74fc76113b154ea44fe93cf0a4e76657
nai08747 <nia.maheshwari@kub.org>
2025-03-18 15:04:11 -0400

    chore: update ember-cli-build, and add dependencies to package.json and pnpm-lock.yaml

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    //ember-cli-build.js
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
+    },
   });
 
   return app.toTree();
